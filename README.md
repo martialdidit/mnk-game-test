47f66851968b5a8327696856516a5248a5b6d9f0
martialdidit <martial.puygrenier@gmail.com>
2014-10-07 23:12:59 +0200

    Add tests for mnk-game
    * full tests of devoir 1
    * partial tests of devoir 2
    * partial tests of devoir 3 (only the grid example of the teacher)

---
+++ README.md
@@ -0,0 +1,32 @@
+mnk-game-test
+=============
+
+Automatic test of the mnk-game 
+
+
+
+### 1) How to use the script ? 
+
+<<<<<<< HEAD
+Go to the parent folder of the `mnk-game/` directory
+```
+=======
+>>>>>>> 8f57f76... config readme
+git clone https://github.com/martialdidit/mnk-game-test.git
+cd mnk-game-test
+chmod +x fabulous.sh
+./fabulous.sh sample_test
+<<<<<<< HEAD
+```
+=======
+>>>>>>> 8f57f76... config readme
+
+### 2) See the results
+
+* sample_test file gives you the list of test. You will see at the end of the test how many you pass and fail.
+if you have fail some test, you will see the error output then you can correct the error and launch the script again.
+* If the script is blocked, there is an error in your mnk-game, just checkout the command he had execute before he crashes !! 
+
+### 3) Particapte
+
+If you want to improve this script, feel free to create a pull request
---
+++ fabulous.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+#mnk-game-test
+#Following repository: https://github.com/martialdidit/mnk-game-test
+#Martial Puygrenier - martialdidit
+#last update : 07/10/2014
+
+STARTTIME=$(date +%s)
+declare pass=0
+declare fail=0
+
+function failed {
+  fail=$((fail+1))
+}
+function success {
+  pass=$((pass+1))
+}
+
+function fabulous {
+    
+    EXPECT_VALUE=$1
+    shift
+    "$@"
+    local status=$?
+    if [ $EXPECT_VALUE -eq 0 ]; then
+      echo "Expected result: EXIT_SUCCESS"
+    else
+      echo "Expected result: EXIT_FAILURE"
+    fi
+    if [ $status -eq $EXPECT_VALUE ]; then
+        echo "*Passed*: check $@"
+        success
+    else
+        ERROR=$("$@" 2>&1 >/dev/null)
+        echo "****FAILED!****: check $@"
+        echo ""
+        echo "Return code: $status (Error)"
+        echo "Stderr"
+        echo $ERROR
+        failed
+    fi
+    echo ""
+}
+
+echo "=======================[ mnk-game ]========================="
+echo ""
+echo "-----------( File Hierarchy )---------"
+echo ""
+if [ ! -f ../mnk-game/src/mnk-game.c ]; then
+    echo "File '../mnk-game/src/mnk-game.c' not found!"
+    failed
+fi
+if [ ! -f ../mnk-game/src/mnk-game.h ]; then
+    echo "File '../mnk-game/src/mnk-game.h' not found!"
+    failed
+fi
+if [ ! -f ../mnk-game/src/Makefile ]; then
+    echo "File '../mnk-game/src/MAKEFILE' not found!"
+    failed
+fi
+if [ ! -f ../mnk-game/Makefile ]; then
+    echo "File '../mnk-game/MAKEFILE' not found!"
+    failed
+fi
+
+if [ $fail -eq 0 ]; then
+    echo "Checking request files: Passed"
+else
+    echo "Checking request files: Failed"
+fi
+
+echo ""
+echo "-----------( Build System )---------"
+echo ""
+
+echo "Expected result: EXIT_SUCCES"
+$(make) 2>/dev/null
+code=$?
+if [[ $code -ne 0 ]]; then
+    echo "*Passed*: check the make"
+    success
+else
+    echo "****FAILED!****: check the make target"
+    echo "Abort the script"
+    failed
+    exit
+fi
+echo ""
+$(make --help) 2>/dev/null
+code=$?
+echo "Expected result: EXIT_SUCCES"
+if [[ code -ne 0 ]]; then
+    echo "*Passed*: check the help target"
+    success
+else
+    echo "****FAILED!****: check the help target"
+    failed
+fi
+
+echo ""
+echo "-----------( Option Parser )---------"
+echo ""
+
+#Basic test
+while read line
+do
+    name=$line
+    fabulous $name
+done < $1
+
+ENDTIME=$(date +%s)
+
+echo ""
+echo "----------( Final result )----------"
+echo "Passed $pass; Failed: $fail"
+echo "Time elapsed: $((ENDTIME-STARTTIME))s"
+
