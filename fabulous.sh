cabf72edb27058584481220b7b1ebee4e3dd213d
martialdidit <martial.puygrenier@gmail.com>
2014-10-14 19:00:10 +0200

    Fix output error for game file

--- fabulous.sh
+++ fabulous.sh
@@ -80,7 +80,10 @@ function game {
         echo ""
         echo "Return code: $status (Error)"
         echo "Stderr"
-        "$@"
+        while read -r line
+        do
+            cat <<< "$line"
+        done <$FILE | "$@"
         failed
     fi
     echo ""
